3d4c6378371a3e9a2caa08a12f083d12474fea6a
KRQPLY <kmichalkorot@gmail.com>
2023-04-01 17:02:31 +0200

    get request

--- src/helpers/getData.js
+++ src/helpers/getData.js
@@ -1,8 +1,16 @@
 import axios from 'axios'
+import { useStorage } from '@vueuse/core'
 
 export default async function getData(endpoint) {
   try {
-    const response = await axios.get(endpoint)
+    const token = useStorage('token', '')
+    const response = await axios.get(endpoint, {
+      headers: {
+        'content-type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        Authorization: `Bearer ${token.value}`
+      }
+    })
 
     return response.data
   } catch (e) {
