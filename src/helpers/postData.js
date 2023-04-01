82cbc620c6ad392cf6370214daa68bd72adc2244
KRQPLY <kmichalkorot@gmail.com>
2023-04-01 17:24:59 +0200

    auth request

--- src/helpers/postData.js
+++ src/helpers/postData.js
@@ -1,14 +1,18 @@
 import axios from 'axios'
 import { useStorage } from '@vueuse/core'
 
-export default async function postData(endpoint, data) {
+export default async function postData(endpoint, data, authorization = false) {
   try {
     const token = useStorage('token', '')
-    const response = await axios.post(endpoint, data, {
-      headers: {
-        Authorization: `Bearer ${token.value}`
+    let config
+    if (authorization) {
+      config = {
+        headers: {
+          Authorization: `Bearer ${token.value}`
+        }
       }
-    })
+    }
+    const response = await axios.post(endpoint, data, config)
 
     return response.data
   } catch (e) {
