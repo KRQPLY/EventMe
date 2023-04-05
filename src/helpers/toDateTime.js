b176d77e6cf457d6b4c03d35364f3a9571cb9ca0
KRQPLY <kmichalkorot@gmail.com>
2023-04-05 22:21:07 +0200

    time sent in miliseconds instead of seconds

--- src/helpers/toDateTime.js
+++ src/helpers/toDateTime.js
@@ -1,5 +1,5 @@
-export default function toDateTime(secs, hour = false) {
-  let myDate = new Date(secs * 1000)
+export default function toDateTime(milis, hour = false) {
+  let myDate = new Date(milis)
   let options = { year: 'numeric', month: 'long', day: 'numeric' }
   if (hour) {
     options = { hour: '2-digit', minute: '2-digit' }
--- src/helpers/toTimeDate.js
+++ src/helpers/toTimeDate.js
@@ -1,3 +1,3 @@
 export default function toDateTime(date) {
-  return new Date(date).getTime() / 1000
+  return new Date(date).getTime()
 }
