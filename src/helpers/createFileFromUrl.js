8a1349a4532c59a55bcac6b10223dfb9b5610b20
KRQPLY <kmichalkorot@gmail.com>
2023-04-21 13:25:01 +0200

    use axios instead of fetch in createFileFromUrl furncion

--- src/helpers/createFileFromUrl.js
+++ src/helpers/createFileFromUrl.js
@@ -1,7 +1,9 @@
+import axios from 'axios'
+
 export default async function createFileFromUrl(url, defaultType = 'image/jpeg') {
   const name = /[^/]*$/.exec(url)[0]
-  const response = await fetch(url)
-  const data = await response.blob()
+  const response = await axios.get(url, { responseType: 'blob' })
+  const data = response.data
 
   return new File([data], name, {
     type: data.type || defaultType
